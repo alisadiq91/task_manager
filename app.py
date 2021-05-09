1706c999c2ca6aa52850ba1cd7c22ad629db89e7
alisadiq91 <ali.sadiq91@gmail.com>
2021-05-09 20:50:25 +0000

    added user profile page

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
@@ -56,6 +57,9 @@ def login():
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
+
             else:
                 flash("Incorrect Username and/or Password")
                 return redirect(url_for("login"))
@@ -64,6 +68,14 @@ def login():
             flash("Incorrect Username and/or Password")
             return redirect(url_for("login"))
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods = ["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username":session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
